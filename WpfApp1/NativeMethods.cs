e4ac2ed0166f4433064b80dd2ea16ab85b933acf
Mikazuki <mikazuki_fuyuno@outlook.com>
2016-12-29 16:10:31 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfApp1/NativeMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// ReSharper disable InconsistentNaming
+
+namespace WpfApp1
+{
+    internal static class NativeMethods
+    {
+        [DllImport("user32")]
+        public static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32")]
+        public static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("gdi32")]
+        public static extern IntPtr CreateDC(string pszDriver, string pszDevice, string pszOutput, IntPtr pInitData);
+
+        [DllImport("gdi32")]
+        public static extern int DeleteDC(IntPtr hDC);
+
+        [DllImport("user32")]
+        public static extern int ReleaceDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("gdi32")]
+        public static extern int BitBlt(IntPtr hDC, int nXDest, int nYDest, int nWidth, int nHeight, IntPtr hdcSource, int nXSource, int nYSource,
+                                        ulong dwRaster);
+    }
+}
